5cff779dc8074cb029eac194ea2d365bb55b95f2
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-23 13:37:25 +0000

    Add beer-list directory, include module, component and template

---
+++ app/beer-list/beer-list.component.js
@@ -0,0 +1,13 @@
+angular
+  .module('beerList')
+  .component('beerList', {
+    templateUrl: 'beer-list/beer-list.template.html',
+    controller: function($http) {
+      let self = this;
+
+      $http.get('/api/beers.json')
+        .then(function(response) { self.beers = response.data })
+        .catch(function(response) { self.beers = [] })
+      },
+    });
+
