c0b3a144be29d2625d24cdd38bc9d19326772b4a
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-23 14:06:21 +0000

    Fix misnamed template url following directory structure change

--- app/components/beer-list/beer-list.component.js
+++ app/components/beer-list/beer-list.component.js
@@ -1,7 +1,7 @@
 angular
   .module('beerList')
   .component('beerList', {
-    templateUrl: 'beer-list/beer-list.template.html',
+    templateUrl: 'components/beer-list/beer-list.template.html',
     controller: function($http) {
       let self = this;
 
