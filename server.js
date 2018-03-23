eeeac4d9b9019d65ca8d9015e31d0e449f6903b5
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-23 13:19:10 +0000

    Expose beers.json under /api route

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'app')))
+app.use('/api', express.static(path.join(__dirname, 'data')));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
